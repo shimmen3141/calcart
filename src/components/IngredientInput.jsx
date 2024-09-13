41f50e65670a9ca6e5bdac0815d21811d1153e39
shimmen <y.shin3141@gmail.com>
2024-09-13 22:14:02 +0900

    関数を下位コンポーネントに移動して整理

--- src/components/IngredientInput.jsx
+++ src/components/IngredientInput.jsx
@@ -3,69 +3,101 @@ import fullWidthToHalfWidth from "../functions/fullWidthToHalfWidth";
 import fractionToDecimal from "../functions/fractionToDecimal";
 import spoonToGram from "../functions/spoonToGram";
 
-const IngredientInput = React.memo(({
-  cart,
-  onChange,
-  cartNumber,
-  cartID,
-  onClear,
-  onRemove,
-}) => {
+const IngredientInput = React.memo(
+  ({
+    allCarts,
+    setAllCarts,
+    cartNumber,
+    cartID,
+  }) => {
+    console.log(`IngredientInput${cartNumber}`);
 
-  console.log(`IngredientInput${cartNumber}`);
+    const [inputText, setInputText] = useState("");
 
-  const [inputText, setInputText] = useState("");
+    // 入力欄の変更により発火する関数
+    const handleInputChange = (event) => {
+      console.log(`InputChange${cartNumber}`);
+      const text = event.target.value;
+      setInputText(text);
 
-  const handleInputChange = (event) => {
-    const text = event.target.value;
-    setInputText(text);
+      const lines = text
+        .split("\n")
+        .map((line) => fullWidthToHalfWidth(line)) // 全角を半角に変換
+        .filter((line) => line.trim()) // 空行を無視
+        .map((line) => line.trim()) // 余計な空白文字を削除
+        .map((line) => fractionToDecimal(line)); // 分数を小数に変換
 
-    const lines = text
-      .split("\n")
-      .filter((line) => line.trim()) // 空行を無視
-      .map((line) => fullWidthToHalfWidth(line)) // 全角を半角に変換
-      .map((line) => fractionToDecimal(line)); // 分数を小数に変換
+      const parsedIngredients = lines
+        .map((line) => {
+          // 最初のスペースで2つに分割
+          const [name, quantity] = line.split(/(?<=^[^\s]+)\s/);
+          const info =
+            quantity !== undefined ? quantity.replace(/\s+/g, "") : "適量";
 
-    const parsedIngredients = lines
-      .map((line) => {
-        // 最初のスペースで2つに分割
-        var [name, quantity] = line.split(/(?<=^[^\s]+)\s/);
-        var info =
-          quantity !== undefined
-            ? quantity.replace(/\s+/g, "")
-            : "適量";
+          const fixedInfo = spoonToGram(name, info);
 
-        info = spoonToGram(name, info);
+          return { name: name, info: fixedInfo };
+        })
+        .filter((ingredient) => ingredient !== null);
 
-        return { name, info };
-      })
-      .filter((ingredient) => ingredient !== null);
+      const newAllCarts = allCarts.map((cart) =>
+        cart.id === cartID
+          ? { ...cart, ingredients: parsedIngredients }
+          : cart
+      );
+      setAllCarts(newAllCarts);
+    };
 
-    onChange(parsedIngredients, cartID);
-  };
+    // クリアボタンの押下により発火する関数
+    const handleClear = () => {
+      setInputText("");
+      const newAllCarts = allCarts.map((cart) =>
+        cart.id === cartID ? { ...cart, ingredients: [] } : cart
+      );
+      setAllCarts(newAllCarts);
+    };
 
-  const handleClear = () => {
-    setInputText("");
-    onClear(cartID);
-  };
+    // ✕ボタンの押下により発火する関数
+    const handleRemove = () => {
+      const newAllCarts = allCarts.filter((cart) => cart.id !== cartID);
+      setAllCarts(newAllCarts);
+    };
 
-  return (
-    <div>
-      <h2>カート{cartNumber + 1}</h2>
-      <div>材料リストを入力：</div>
-      <textarea
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder={`例:\nにんじん 2本\nタマネギ 3個\n醤油 50ml`}
-        rows="30"
-        cols="50"
-      />
+    // カート台数のスピンボタン変更時に発火する関数
+    const hadleCartCountChange = (event) => {
+      const newAllCarts = allCarts.map((cart) =>
+        cart.id === cartID
+          ? { ...cart, cartCount: Number(event.target.value) }
+          : cart
+      );
+      setAllCarts(newAllCarts);
+    };
+
+    return (
       <div>
-        <button onClick={handleClear}>クリア</button>
-        <button onClick={() => onRemove(cartID)}>×</button>
+        <h2>カート{cartNumber + 1}</h2>
+        <div>材料リストを入力：</div>
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder={`例:\nにんじん 2本\nタマネギ 3個\n醤油 50ml`}
+          rows="30"
+          cols="50"
+        />
+        <div>
+          <button onClick={handleClear}>クリア</button>
+          <button onClick={handleRemove}>×</button>
+        </div>
+        <div>カート台数：</div>
+        <input
+          type="number"
+          min="0"
+          defaultValue="1"
+          onChange={hadleCartCountChange} 
+        />
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default IngredientInput;
--- src/components/IngredientsContainer.jsx
+++ src/components/IngredientsContainer.jsx
@@ -6,32 +6,6 @@ const IngredientsContainer = ({ allCarts, setAllCarts }) => {
 
   console.log("IngredientsContainer");
 
-  const handleIngredientChange = (id, newIngredients) => {
-    const newAllCarts = allCarts.map((cart) =>
-      cart.id === id ? { ...cart, ingredients: newIngredients } : cart
-    );
-    setAllCarts(newAllCarts);
-  };
-
-  const handleCartCountChange = (id, newCartCount) => {
-    const newAllCarts = allCarts.map((cart) =>
-      cart.id === id ? { ...cart, cartCount: newCartCount } : cart
-    );
-    setAllCarts(newAllCarts);
-  };
-
-  const handleClearInput = (id) => {
-    const newAllCarts = allCarts.map((cart) =>
-      cart.id === id ? { ...cart, ingredients: [] } : cart
-    );
-    setAllCarts(newAllCarts);
-  };
-
-  const handleRemoveInput = (id) => {
-    const newAllCarts = allCarts.filter((cart) => cart.id !== id);
-    setAllCarts(newAllCarts);
-  };
-
   const handleAddInput = () => {
     setAllCarts([
       ...allCarts,
@@ -45,23 +19,10 @@ const IngredientsContainer = ({ allCarts, setAllCarts }) => {
         {allCarts.map((cart, index) => (
           <div key={cart.id} className="input-group">
             <IngredientInput
-              cart={cart}
+              allCarts={allCarts}
+              setAllCarts={setAllCarts}
               cartID={cart.id}
               cartNumber={index}
-              onChange={(newIngredients) =>
-                handleIngredientChange(cart.id, newIngredients)
-              }
-              onClear={() => handleClearInput(cart.id)}
-              onRemove={() => handleRemoveInput(cart.id)}
-            />
-            <div>カート台数：</div>
-            <input
-              type="number"
-              min="0"
-              defaultValue="1"
-              onChange={(event) =>
-                handleCartCountChange(cart.id, Number(event.target.value))
-              }
             />
           </div>
         ))}
