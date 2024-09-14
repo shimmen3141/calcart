ae7bfce9a67a58cb65436dc63cf63ad307f81431
shimmen3141 <y.shin3141@gmail.com>
2024-09-14 18:52:01 +0900

    refac:CopyButtonの処理をカスタムフックに切り分け

--- src/components/CopyButton/CopyButton.js
+++ src/components/CopyButton/CopyButton.js
@@ -1,22 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useCopyButton from './useCopyButton';
 
 const CopyButton = ({ text }) => {
 
-    // ボタンの表示テキストを管理する変数
-    const [copyStatus, setCopyStatus] = useState('コピー');
-
-    const handleCopy = () => {
-        navigator.clipboard.writeText(text)
-            .then(() => {
-                // コピー成功時にボタンのテキストを変更
-                setCopyStatus('コピー完了！');
-                // 2.5秒後に元のテキストに戻す
-                setTimeout(() => setCopyStatus('コピー'), 2500);
-            })
-            .catch(err => {
-                console.error('コピーに失敗しました:', err);
-            });
-    };
+    const { copyStatus, handleCopy } = useCopyButton({ text });
 
     return (
         <button onClick={handleCopy}>
---
+++ src/components/CopyButton/useCopyButton.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useCopyButton = ({ text }) => {
+  // ボタンの表示テキストを管理する変数
+  const [copyStatus, setCopyStatus] = useState("コピー");
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        // コピー成功時にボタンのテキストを変更
+        setCopyStatus("コピー完了！");
+        // 2.5秒後に元のテキストに戻す
+        setTimeout(() => setCopyStatus("コピー"), 2500);
+      })
+      .catch((err) => {
+        console.error("コピーに失敗しました:", err);
+      });
+  };
+
+  return { copyStatus, handleCopy };
+};
+
+export default useCopyButton;
