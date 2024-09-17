ff08a244c41c15cc31d9c556e09f47b619589c48
shimmen3141 <y.shin3141@gmail.com>
2024-09-18 04:19:59 +0900

    update:カートの追加・削除時に自動スクロールとアニメーションを追加

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,42 +1,58 @@
 import React from "react";
 import useCart from "./useCart";
 
-const Cart = React.memo(({ allCarts, setAllCarts, cartNumber, cartID }) => {
-  const {
-    inputText,
-    handleInputChange,
-    handleClear,
-    handleRemove,
-    isLastCart,
-    hadleCartCountChange,
-  } = useCart({ allCarts, setAllCarts, cartID });
+const Cart = React.memo(
+  ({
+    allCarts,
+    setAllCarts,
+    cartID,
+    cartNumber,
+    cartRefs,
+  }) => {
+    const {
+      inputText,
+      handleInputChange,
+      handleClear,
+      handleRemove,
+      isLastCart,
+      hadleCartCountChange,
+    } = useCart({
+      allCarts,
+      setAllCarts,
+      cartID,
+      cartNumber,
+      cartRefs,
+    });
 
-  console.log(`Cart${cartNumber}`);
+    console.log(`Cart${cartNumber}`);
 
-  return (
-    <div>
-      <h2>カート{cartNumber + 1}</h2>
-      <div>材料リストを入力：</div>
-      <textarea
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder={`例:\nにんじん 2本\nタマネギ 3個\n醤油 50ml`}
-        rows="30"
-        cols="50"
-      />
+    return (
       <div>
-        <button onClick={handleClear}>クリア</button>
-        <button onClick={handleRemove} disabled={isLastCart()}>×</button>
+        <h2>カート{cartNumber + 1}</h2>
+        <div>材料リストを入力：</div>
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder={`例:\nにんじん 2本\nタマネギ 3個\n醤油 50ml`}
+          rows="30"
+          cols="50"
+        />
+        <div>
+          <button onClick={handleClear}>クリア</button>
+          <button onClick={handleRemove} disabled={isLastCart()}>
+            ×
+          </button>
+        </div>
+        <div>カート台数：</div>
+        <input
+          type="number"
+          min="0"
+          defaultValue="1"
+          onChange={hadleCartCountChange}
+        />
       </div>
-      <div>カート台数：</div>
-      <input
-        type="number"
-        min="0"
-        defaultValue="1"
-        onChange={hadleCartCountChange}
-      />
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Cart;
