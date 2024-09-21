19d3b0a570046b86464b1866821dcf149a3a9be7
shimmen <y.shin3141@gmail.com>
2024-09-22 03:23:30 +0900

    update:removeSymbolsを修正

--- src/functions/removeSymbols.js
+++ src/functions/removeSymbols.js
@@ -1,12 +1,15 @@
 // 文字列先頭から括弧や記号を消去する関数
 export default function removeSymbols(str) {
-  // 文字列先頭の()に囲まれた内容を消去する
-  const strWithoutParentheses = str.replace(/^\s*\(.*?\)\s*/, "");
-  // カタカナ、ひらがな、漢字、アルファベット、数字以外の記号を消去する
-  const strWithoutSymbols = strWithoutParentheses.replace(
-    /^[^\u30a0-\u30ff\u3040-\u309f\u4e00-\u9fcfA-Za-z0-9]+/,
-    ""
-  );
 
-  return strWithoutSymbols;
+  const parenthesesRegex = /^\s*\(.*?\)\s*/;
+  const symbolsRegex = /^[^\u30a0-\u30ff\u3040-\u309f\u4e00-\u9fcfA-Za-z0-9()]+/;
+
+  while ( parenthesesRegex.test(str) || symbolsRegex.test(str) ) {
+    // 文字列先頭の()に囲まれた内容を消去する
+    str = str.replace(parenthesesRegex, "");
+    // カタカナ、ひらがな、漢字、アルファベット、数字、()以外の記号を消去する
+    str = str.replace(symbolsRegex, "");
+  }
+
+  return str;
 };
