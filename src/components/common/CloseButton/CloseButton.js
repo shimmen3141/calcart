e73f7ecb80034703bbacb50e42f6cd6a0f0cb583
shimmen <y.shin3141@gmail.com>
2024-09-25 20:02:27 +0900

    add:CloseButtonを追加

---
+++ src/components/common/CloseButton/CloseButton.js
@@ -0,0 +1,11 @@
+import "./CloseButton.css";
+
+const CloseButton = ({ onClick, disabled }) => {
+  return (
+    <div className="closeButton" onClick={onClick} disabled={disabled}>
+      ×
+    </div>
+  );
+};
+
+export default CloseButton;
