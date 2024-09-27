9bd8f47b0b98f3edab4ef69aa31614685480b3b8
shimmen <y.shin3141@gmail.com>
2024-09-27 16:38:45 +0900

    add:MainContentsを追加し、CartsとShoppingListをまとめる

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { AppHeader, AppFooter, ShoppingList, Carts } from "./components/index";
+import { AppHeader, MainContents, AppFooter } from "./components/index";
 import { ToggleSwitchProvider } from "./contexts/ToggleSwitchContext";
 
 const App = () => {
-  const [allCarts, setAllCarts] = useState([
-    { id: Date.now(), ingredients: [], cartCount: 1 },
-    { id: Date.now() + 1, ingredients: [], cartCount: 1 },
-  ]);
-
-  const organizeIngredient = () => {
-    return allCarts
-      .filter((cart) => cart.cartCount > 0)
-      .flatMap((cart) =>
-        cart.ingredients.map((ingredient) => ({
-          name: ingredient.name,
-          info: ingredient.info,
-          cartCount: cart.cartCount,
-        }))
-      );
-  };
-
   return (
     <ToggleSwitchProvider>
       <AppHeader />
-      <Carts allCarts={allCarts} setAllCarts={setAllCarts} />
-      <ShoppingList items={organizeIngredient()} />
+      <MainContents />
       <AppFooter />
     </ToggleSwitchProvider>
   );
