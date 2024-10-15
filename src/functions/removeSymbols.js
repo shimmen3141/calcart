5d1c25fba4b32e3ee7fc08a45817d14c97d9f099
shimmen3141 <y.shin3141@gmail.com>
2024-10-15 14:51:48 +0900

    update:vegetableExceptionListを用いた処理を実装

--- src/functions/removeSymbols.js
+++ src/functions/removeSymbols.js
@@ -1,8 +1,10 @@
 // 文字列先頭から括弧や記号を消去する関数
 export default function removeSymbols(str) {
+  // ()とその中身の正規表現
   const parenthesesRegex = /^\s*\(.*?\)\s*/;
+  // カタカナ、ひらがな、漢字、数字、()以外の正規表現
   const symbolsRegex =
-    /^[^\u30a0-\u30ff\u3040-\u309f\u4e00-\u9fcfA-Za-z0-9()]+/;
+    /^[^\u30a0-\u30ff\u3040-\u309f\u4e00-\u9fcf0-9()]+/;
 
   while (parenthesesRegex.test(str) || symbolsRegex.test(str)) {
     // 文字列先頭の()に囲まれた内容を消去する
