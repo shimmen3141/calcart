13fbd59ea55cdfd28f7479451ca0fc2b15dbc026
shimmen <y.shin3141@gmail.com>
2024-10-23 01:08:43 +0900

    update:SettingButtonをAppHeaderに移動

--- src/features/AppHeader/AppHeader.js
+++ src/features/AppHeader/AppHeader.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./AppHeader.css";
+import { ToggleSwitchArea } from "../index";
+import Accordion from "../../components/Accordion/Accordion";
+import SettingButton from "../../components/SettingButton/SettingButton";
+import useAccordion from "../../components/Accordion/useAccordion";
 
 const AppHeader = () => {
+// アコーディオンの開閉を管理する変数
+  const [isOpen, setIsOpen] = useState(false);
+
+  // アコーディオンの開閉を切り替える関数
+  const toggleAccordion = () => {
+    setIsOpen((prevStates) => !prevStates);
+  };
+  //const { toggleAccordion } = useAccordion();
   return (
     <div className="appHeader">
-      Calcart
+      <div className="title">Calcart</div>
+      <SettingButton onClick={toggleAccordion} />
+      <Accordion isOpen={isOpen}>
+        <ToggleSwitchArea />
+      </Accordion>
     </div>
   );
 };
