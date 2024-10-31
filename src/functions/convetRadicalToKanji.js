4820a704990419f8e65572cc7f69c64d0bd4d828
shimmen3141 <y.shin3141@gmail.com>
2024-10-31 21:27:11 +0900

    add:convertRadicalToKanjiを追加

---
+++ src/functions/convetRadicalToKanji.js
@@ -0,0 +1,13 @@
+import { radicalToKanjiMap } from "../data/radicalToKanjiMap";
+
+/* 
+    CJK部首、康煕部首を通常の漢字に変換する関数
+    (PDFをコピーすると、漢字が部首文字に化ける場合があるため)
+    参考：https://github.com/yamamaya/radicalchar
+*/
+export default function convertRadicalToKanji(str) {
+  // 部首文字と漢字の連想配列を用いて変換
+  return Array.from(str)
+    .map((char) => radicalToKanjiMap[char] || char)
+    .join("");
+}
