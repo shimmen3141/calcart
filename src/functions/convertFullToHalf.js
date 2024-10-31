2726e163f9717ec61f291e84054090bcb4309362
shimmen3141 <y.shin3141@gmail.com>
2024-10-31 21:52:34 +0900

    update:convertFullToHalf内の表記などを修正

--- src/functions/convertFullToHalf.js
+++ src/functions/convertFullToHalf.js
@@ -1,17 +1,18 @@
 // 全角の記号、数字、アルファベット等を半角に変換する関数
 export default function convertFullToHalf(str) {
   // 半角変換
-  var halfVal = str.replace(/[！-～]/g, function (tmpStr) {
-    // 文字コードをシフト
-    return String.fromCharCode(tmpStr.charCodeAt(0) - 0xfee0);
-  });
-
-  // 文字コードシフトで対応できない文字の変換
-  return halfVal
+  const convertedStr = str
+    .replace(/[！-～]/g, function (tmpStr) {
+      // 文字コードをシフト
+      return String.fromCharCode(tmpStr.charCodeAt(0) - 0xfee0);
+    })
+    // 文字コードシフトで対応できない文字の変換
     .replace(/”/g, '"')
     .replace(/’/g, "'")
     .replace(/‘/g, "`")
     .replace(/￥/g, "\\")
     .replace(/　/g, " ")
     .replace(/〜/g, "~");
+
+  return convertedStr;
 }
