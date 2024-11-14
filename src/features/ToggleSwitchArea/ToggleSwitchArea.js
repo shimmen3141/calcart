9a0e0f1ac3aa813a6c64a159eb664640965a5b63
shimmen3141 <y.shin3141@gmail.com>
2024-11-14 16:02:00 +0900

    update:labeledToggleSwitchのcssを改善 #18

--- src/features/ToggleSwitchArea/ToggleSwitchArea.js
+++ src/features/ToggleSwitchArea/ToggleSwitchArea.js
@@ -24,34 +24,36 @@ const ToggleSwitchArea = () => {
         labelText="大さじ・小さじ → g に変換"
       /> */}
       <div className="triangle"></div>
-      <div className="labeledToggleSwitch_top">
-        <div className="labelText">記号を消去</div>
-        <HelpButton onClick={() => handleOpenModal("removeSymbols")} />
-        <RemoveSymbolsModal isOpen={isModalOpen} onClose={handleCloseModal} />
-        <ToggleSwitch
-          id="removeSymbols"
-          isChecked={toggleStates.removeSymbols}
-          onChange={handleSwitchChange}
-        />
-      </div>
-      <div className="labeledToggleSwitch">
-        <div className="labelText">大さじ・小さじをグラムに変換</div>
-        <HelpButton onClick={() => handleOpenModal("spoonToGram")} />
-        <SpoonToGramModal isOpen={isModalOpen} onClose={handleCloseModal} />
-        <ToggleSwitch
-          id="spoonToGram"
-          isChecked={toggleStates.spoonToGram}
-          onChange={handleSwitchChange}
-        />
-      </div>
-      <div className="labeledToggleSwitch_bottom">
-        <div className="labelText">リストを分割</div>
-        <HelpButton onClick={() => handleOpenModal("spoonToGram")} />
-        <ToggleSwitch
-          id="classifyItems"
-          isChecked={toggleStates.classifyItems}
-          onChange={handleSwitchChange}
-        />
+      <div className="labeledToggleSwitches">
+        <div className="labeledToggleSwitch">
+          <div className="labelText">記号を消去</div>
+          <HelpButton onClick={() => handleOpenModal("removeSymbols")} />
+          <RemoveSymbolsModal isOpen={isModalOpen} onClose={handleCloseModal} />
+          <ToggleSwitch
+            id="removeSymbols"
+            isChecked={toggleStates.removeSymbols}
+            onChange={handleSwitchChange}
+          />
+        </div>
+        <div className="labeledToggleSwitch">
+          <div className="labelText">大さじ・小さじをグラムに変換</div>
+          <HelpButton onClick={() => handleOpenModal("spoonToGram")} />
+          <SpoonToGramModal isOpen={isModalOpen} onClose={handleCloseModal} />
+          <ToggleSwitch
+            id="spoonToGram"
+            isChecked={toggleStates.spoonToGram}
+            onChange={handleSwitchChange}
+          />
+        </div>
+        <div className="labeledToggleSwitch">
+          <div className="labelText">リストを分割</div>
+          <HelpButton onClick={() => handleOpenModal("spoonToGram")} />
+          <ToggleSwitch
+            id="classifyItems"
+            isChecked={toggleStates.classifyItems}
+            onChange={handleSwitchChange}
+          />
+        </div>
       </div>
     </div>
   );
