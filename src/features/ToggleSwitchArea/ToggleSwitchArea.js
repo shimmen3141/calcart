23f27853d74f6c9f08c5159299f6e8a89fa92221
shimmen3141 <y.shin3141@gmail.com>
2024-12-07 11:52:42 +0900

    update:DropDownMenuの吹き出し部分にborderを追加 #59

--- src/features/ToggleSwitchArea/ToggleSwitchArea.js
+++ src/features/ToggleSwitchArea/ToggleSwitchArea.js
@@ -6,6 +6,7 @@ const ToggleSwitchArea = () => {
 
   return (
     <div className="toggleSwitchArea">
+      <div className="triangleBorder"></div>
       <div className="triangle"></div>
       <div className="labeledToggleSwitches">
         <LabeledToggleSwitch id="removeSymbols" labelText="記号を消去" />
