f1cf9743c2765a58ab18f8474d2ffad3f18340b0
shimmen3141 <y.shin3141@gmail.com>
2025-02-06 04:50:23 +0900

    update:uiディレクトリを作成し、共通部品を移動 #69

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,13 @@
-export { default as CloseButton } from "./CloseButton/CloseButton";
-export { default as CopyButton } from "./CopyButton/CopyButton";
-export { default as DropdownMenu } from "./DropdownMenu/DropdownMenu";
-export { default as useDropdownMenu } from "./DropdownMenu/useDropdownMenu";
-export { default as HelpButton } from "./HelpButton/HelpButton";
-export { default as Modal } from "./Modal/Modal";
-export { default as SettingButton } from "./SettingButton/SettingButton";
-export { default as ToggleSwitch } from "./ToggleSwitch/ToggleSwitch";
+// layout
+export { default as CloseButton } from "./ui/CloseButton/CloseButton";
+export { default as CopyButton } from "./ui/CopyButton/CopyButton";
+export { default as DropdownMenu } from "./ui/DropdownMenu/DropdownMenu";
+export { default as useDropdownMenu } from "./ui/DropdownMenu/useDropdownMenu";
+export { default as HelpButton } from "./ui/HelpButton/HelpButton";
+export { default as Modal } from "./ui/Modal/Modal";
+export { default as SettingButton } from "./ui/SettingButton/SettingButton";
+export { default as ToggleSwitch } from "./ui/ToggleSwitch/ToggleSwitch";
 
-
+// ui
 export { default as AppFooter } from "./layout/AppFooter/AppFooter";
 export { default as AppHeader } from "./layout/AppHeader/AppHeader";
