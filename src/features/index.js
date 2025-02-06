88ce0c71b0cb66fe978ee77e198c7c990a446333
shimmen3141 <y.shin3141@gmail.com>
2025-02-06 17:15:03 +0900

    move:LabeledToggleSwitchをSetting/componentsに移動 #73

--- src/features/index.js
+++ src/features/index.js
@@ -4,9 +4,11 @@ export { default as Cart } from "./Cart/Cart";
 export { default as CartCountSpinButton } from "./CartCountSpinButton/CartCountSpinButton";
 export { default as Carts } from "./Carts/Carts";
 export { default as InputFormatTag } from "./InputFormatTag/InputFormatTag";
-export { default as LabeledToggleSwitch } from "./LabeledToggleSwitch/LabeledToggleSwitch";
 export { default as ShoppingList } from "./ShoppingList/ShoppingList";
 export { default as ClassifiedShoppingList } from "./ShoppingList/ClassifiedShoppingList";
 export { default as EmptyShoppingList } from "./ShoppingList/EmptyShoppingList";
 export { default as ShoppingListArea } from "./ShoppingListArea/ShoppingListArea";
+
+// Setting
+export { default as LabeledToggleSwitch } from "./Setting/components/LabeledToggleSwitch/LabeledToggleSwitch";
 export { default as ToggleSwitchArea } from "./Setting/components/ToggleSwitchArea/ToggleSwitchArea";
