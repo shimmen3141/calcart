e9deb2a1e28b2627aea6af476c60f4c40c238660
shimmen3141 <y.shin3141@gmail.com>
2025-02-06 17:24:43 +0900

    add:SettingMenu.jsを追加 #73

---
+++ src/features/Setting/components/SettingMenu/SettingMenu.js
@@ -0,0 +1,20 @@
+import { ToggleSwitchArea } from "features";
+import { DropdownMenu, useDropdownMenu, SettingButton } from "components";
+
+const SettingMenu = () => {
+  const { isOpen, toggleDropdown } = useDropdownMenu();
+
+  return (
+    <div>
+      <div className={`settingButton ${isOpen ? "open" : ""}`}>
+        <SettingButton onClick={toggleDropdown} />
+      </div>
+      <DropdownMenu isOpen={isOpen}>
+        <ToggleSwitchArea />
+      </DropdownMenu>
+      {isOpen && <div className="outsideMenu" onClick={toggleDropdown}></div>}
+    </div>
+  );
+};
+
+export default SettingMenu;
