96e13e72db09b84f1047ae5935df7dbd243fbe76
shimmen3141 <y.shin3141@gmail.com>
2025-02-06 13:45:03 +0900

    update:コメントのミスを修正

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 // layout
+export { default as AppFooter } from "./layout/AppFooter/AppFooter";
+export { default as AppHeader } from "./layout/AppHeader/AppHeader";
+
+// ui
 export { default as CloseButton } from "./ui/CloseButton/CloseButton";
 export { default as CopyButton } from "./ui/CopyButton/CopyButton";
 export { default as DropdownMenu } from "./ui/DropdownMenu/DropdownMenu";
@@ -7,7 +11,3 @@ export { default as HelpButton } from "./ui/HelpButton/HelpButton";
 export { default as Modal } from "./ui/Modal/Modal";
 export { default as SettingButton } from "./ui/SettingButton/SettingButton";
 export { default as ToggleSwitch } from "./ui/ToggleSwitch/ToggleSwitch";
-
-// ui
-export { default as AppFooter } from "./layout/AppFooter/AppFooter";
-export { default as AppHeader } from "./layout/AppHeader/AppHeader";
