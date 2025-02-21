527cc118cd5aad63a367f7d8be4f9cb4c3516741
shimmen3141 <y.shin3141@gmail.com>
2025-02-21 16:43:35 +0900

    add:BaseShoppingListを作成 #81

---
+++ src/features/ShoppingList/components/ShoppingLists/BaseShoppingList.js
@@ -0,0 +1,18 @@
+const BaseShoppingList = ({ items, className, labelName }) => {
+  if (items.length === 0) return null;
+
+  return (
+    <div>
+      <ul className={className}>
+        {labelName && <div className="listTypeTag">{labelName}</div>}
+        {items.map((item, index) => (
+          <div key={index}>
+            <input type="checkbox" /> {item.name} {item.info}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BaseShoppingList;
