66de8c823db3755572f3bcc89107b83b33314911
shimmen3141 <y.shin3141@gmail.com>
2025-02-25 19:17:41 +0900

    add:parseCartsToItemsのファイルを追加 #87

---
+++ src/features/ShoppingList/utils/textProcessing/parseCartsToItems.js
@@ -0,0 +1,45 @@
+import {
+  splitTextByLinebreak,
+  cleanLines,
+  classifyInputFormat,
+  parseLinesToItems,
+  convertSpoonToGram,
+  multiplyQuantities,
+} from "functions";
+import mergeDuplicateItems from "features/ShoppingList/utils/textProcessing/mergeDuplicateItems";
+
+const parseCartsToItems = (
+  carts,
+  options = { removeSymbols: true, spoonToGram: true }
+) => {
+  const items = carts
+    .filter((cart) => cart.count > 0)
+    .flatMap((cart) => {
+      // 入力内容を改行ごとに分割する
+      const lines = splitTextByLinebreak(cart.inputText);
+      // 各行をクリーニングする (オプションに応じて記号除去)
+      const cleanedLines = cleanLines(lines, options.removeSymbols);
+      // 入力形式を分類する
+      const inputFormat = classifyInputFormat(cleanedLines);
+      // 行をオブジェクトに変換する
+      let items = parseLinesToItems(cleanedLines, inputFormat);
+
+      // spoonToGram オプションが true の場合のみ変換
+      if (options.spoonToGram) {
+        items = items.map((item) => ({
+          ...item,
+          info: convertSpoonToGram(item.name, item.info),
+        }));
+      }
+
+      // カートの個数に応じて数量を乗算
+      return items.map((item) => ({
+        ...item,
+        info: multiplyQuantities(item.info, cart.count),
+      }));
+    });
+  // 重複アイテムをマージ
+  return mergeDuplicateItems(items);
+};
+
+export default parseCartsToItems;
