fb554c66a37128f42008f0239803e190f6339487
shimmen3141 <y.shin3141@gmail.com>
2025-02-28 18:25:58 +0900

    update:コピーボタンにアイコンを追加 #98

--- src/components/ui/CopyButton/CopyButton.js
+++ src/components/ui/CopyButton/CopyButton.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import useCopyButton from './useCopyButton';
+import React from "react";
+import useCopyButton from "./useCopyButton";
 import "./CopyButton.scss";
 
 const CopyButton = ({ text }) => {
+  const { copyStatus, handleCopy } = useCopyButton({ text });
 
-    const { copyStatus, handleCopy } = useCopyButton({ text });
-
-    return (
-        <button className="copyButton" onClick={handleCopy}>
-            {copyStatus}
-        </button>
-    );
+  return (
+    <div class="copy-icon-wrapper" onClick={handleCopy}>
+      <div class="copy-icon-background"></div>
+      <div class="dli-copy">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default CopyButton;
