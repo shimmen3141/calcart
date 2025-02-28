c73f1fb1a2d04be633d695df77ceafc67b82b761
shimmen3141 <y.shin3141@gmail.com>
2025-02-28 18:59:13 +0900

    update:CopyIconを切り分け #98

--- src/components/ui/CopyButton/CopyButton.js
+++ src/components/ui/CopyButton/CopyButton.js
@@ -1,16 +1,15 @@
 import React from "react";
 import useCopyButton from "./useCopyButton";
+import CopyIcon from "./CopyIcon";
 import "./CopyButton.scss";
 
 const CopyButton = ({ text }) => {
   const { copyStatus, handleCopy } = useCopyButton({ text });
 
   return (
-    <div class="copy-icon-wrapper" onClick={handleCopy}>
-      <div class="copy-icon-background"></div>
-      <div class="dli-copy">
-        <div></div>
-      </div>
+    <div className="copyButton" onClick={handleCopy}>
+      <CopyIcon />
+      <div className="copyStatus">{copyStatus}</div>
     </div>
   );
 };
---
+++ src/components/ui/CopyButton/CopyIcon.js
@@ -0,0 +1,14 @@
+import "./CopyIcon.scss";
+
+const CopyIcon = () => {
+  return (
+    <div className="copy-icon-wrapper">
+      <div className="copy-icon-background"></div>
+      <div className="dli-copy">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default CopyIcon;
