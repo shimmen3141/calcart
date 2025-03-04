ac50ccc46b7f27f942ab5e10954470a56e58f1d7
shimmen3141 <y.shin3141@gmail.com>
2025-03-05 03:01:36 +0900

    add:CartIconを追加 #55

---
+++ src/features/Carts/components/Cart/CartIcon.js
@@ -0,0 +1,20 @@
+import { useCart } from "contexts";
+
+const CartIcon = ({ cartId }) => {
+  const { getCartCount, getCartInputText } = useCart();
+
+  const cartCount = getCartCount(cartId);
+  const cartInputText = getCartInputText(cartId);
+
+  // cartInputText が空なら"cart_empty"、そうでなければ"cart_full"
+  const baseName = cartInputText === "" ? "cart_empty" : "cart_full";
+
+  // cartCount が3以上なら "3"、0～2なら cartCount の数字
+  const displayCount = cartCount >= 3 ? 3 : cartCount;
+  
+  const imageName = `${baseName}_${displayCount}.png`;
+
+  return <img src={${imageName}} alt="Cart Icon" />;
+};
+
+export default CartIcon;
