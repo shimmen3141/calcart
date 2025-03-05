b0e95929431149d40218b5588f5d4f96efab70aa
shimmen3141 <y.shin3141@gmail.com>
2025-03-05 17:46:00 +0900

    update:CloseButtonでCrossIconを用いる #55

--- src/components/ui/CloseButton/CloseButton.js
+++ src/components/ui/CloseButton/CloseButton.js
@@ -1,8 +1,11 @@
+import CrossIcon from "./CrossIcon";
 import "./CloseButton.scss";
 
 const CloseButton = ({ onClick, disabled }) => {
   return (
-    <button className="closeButton" onClick={onClick} disabled={disabled}></button>
+    <button className="closeButton" onClick={onClick} disabled={disabled}>
+      <CrossIcon />
+    </button>
   );
 };
 
