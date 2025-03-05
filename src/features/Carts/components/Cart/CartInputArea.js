5fed99a41f734f7c513695a1e31f95d0becf6d0c
shimmen3141 <y.shin3141@gmail.com>
2025-03-06 03:55:31 +0900

    update:CartInputAreaの色等を変更 #55

--- src/features/Carts/components/Cart/CartInputArea.js
+++ src/features/Carts/components/Cart/CartInputArea.js
@@ -12,12 +12,14 @@ const CartInputArea = ({ cartId }) => {
       <div className="triangleBorder"></div>
       <div className="triangle"></div>
       <InputFormatTag inputText={inputText} />
-      <div>材料リストを入力：</div>
-      <textarea
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder={`---例1 (1行1材料)---\nにんじん 2本\nタマネギ 3個\n醤油 50ml\n\n---例2 (2行1材料)---\nにんじん\n2本\nタマネギ\n3個\n醤油\n50ml`}
-      />
+      <div className="text">アイテムリストを入力：</div>
+      <div className="textareaWrapper">
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder={`---例1 (1行1材料)---\nにんじん 2本\nタマネギ 3個\n醤油 50ml\n\n---例2 (2行1材料)---\nにんじん\n2本\nタマネギ\n3個\n醤油\n50ml`}
+        />
+      </div>
       <button className="clearButton" onClick={handleClear}>
         クリア
       </button>
