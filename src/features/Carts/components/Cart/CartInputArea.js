86a9ba00193e462d081be6c2fed93fe3ab4b73c7
shimmen3141 <y.shin3141@gmail.com>
2025-03-06 03:05:26 +0900

    remove:余計なdivを削除 #55

--- src/features/Carts/components/Cart/CartInputArea.js
+++ src/features/Carts/components/Cart/CartInputArea.js
@@ -18,11 +18,9 @@ const CartInputArea = ({ cartId }) => {
         onChange={handleInputChange}
         placeholder={`---例1 (1行1材料)---\nにんじん 2本\nタマネギ 3個\n醤油 50ml\n\n---例2 (2行1材料)---\nにんじん\n2本\nタマネギ\n3個\n醤油\n50ml`}
       />
-      <div>
-        <button className="clearButton" onClick={handleClear}>
-          クリア
-        </button>
-      </div>
+      <button className="clearButton" onClick={handleClear}>
+        クリア
+      </button>
     </div>
   );
 };
