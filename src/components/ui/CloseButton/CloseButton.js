83b7850b1f8ceb355950576ac14b54fc329c282e
shimmen3141 <y.shin3141@gmail.com>
2025-03-05 18:05:24 +0900

    update:CloseButtonに丸いvariantを追加 #55

--- src/components/ui/CloseButton/CloseButton.js
+++ src/components/ui/CloseButton/CloseButton.js
@@ -1,9 +1,13 @@
 import CrossIcon from "./CrossIcon";
 import "./CloseButton.scss";
 
-const CloseButton = ({ onClick, disabled }) => {
+const CloseButton = ({ onClick, disabled, variant = "square" }) => {
   return (
-    <button className="closeButton" onClick={onClick} disabled={disabled}>
+    <button
+      className={`closeButton closeButton--${variant}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <CrossIcon />
     </button>
   );
