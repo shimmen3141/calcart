e583612df382f825af9c311d0e2f81382d6d48e5
shimmen3141 <y.shin3141@gmail.com>
2025-03-09 02:06:20 +0900

    update:featuresのエントリポイントを作成 #96

--- src/features/index.js
+++ src/features/index.js
@@ -1,25 +1,3 @@
-// Carts
-export { default as CartsContainer } from "./Carts/components/CartsContainer";
-export { default as Carts } from "./Carts/components/Carts";
-export { default as CartsHeader } from "./Carts/components/CartsHeader";
-export { default as AddCartButton } from "./Carts/components/AddCartButton";
-export { default as Cart } from "./Carts/components/Cart/Cart";
-export { default as CartCountSpinButton } from "./Carts/components/Cart/CartCountSpinButton";
-export { default as CartIcon } from "./Carts/components/Cart/CartIcon";
-export { default as CartInputArea } from "./Carts/components/Cart/CartInputArea";
-export { default as InputFormatTag } from "./Carts/components/Cart/InputFormatTag";
-
-// ShoppingList
-export { default as ShoppingListContainer } from "./ShoppingList/components/ShoppingListContainer";
-export { default as StandardShoppingList } from "./ShoppingList/components/ShoppingLists/StandardShoppingList";
-export { default as ClassifiedShoppingList } from "./ShoppingList/components/ShoppingLists/ClassifiedShoppingList";
-export { default as EmptyShoppingList } from "./ShoppingList/components/ShoppingLists/EmptyShoppingList";
-
-// Setting
-export { default as LabeledToggleSwitch } from "./Setting/components/LabeledToggleSwitch";
-export { default as SettingMenu } from "./Setting/components/SettingMenu";
-export { default as ToggleSwitchArea } from "./Setting/components/ToggleSwitchArea";
-export {
-  SettingToggleProvider,
-  useSettingToggle,
-} from "./Setting/contexts/SettingToggleContext";
+export * from "./Carts";
+export * from "./Setting";
+export * from "./ShoppingList";
