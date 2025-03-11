6a49a5de2dc79b0a01b7b1b39434a95cbb1cb811
shimmen3141 <y.shin3141@gmail.com>
2025-03-11 19:19:26 +0900

    update:アイコンを修正し、AppHeaderに追加 #124

--- src/components/layout/AppHeader/AppHeader.js
+++ src/components/layout/AppHeader/AppHeader.js
@@ -6,6 +6,11 @@ const AppHeader = () => {
 
   return (
     <div className="appHeader">
+      <img
+        src={`${process.env.PUBLIC_URL}/cart_icon_transparent.png`}
+        alt="App Header Icon"
+        className="appHeaderIcon"
+      />
       <div className="title">Calcart</div>
       <SettingMenu />
     </div>
