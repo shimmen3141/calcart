de75af1ea7205ac4ebfde701bd4b1b990dbed0ac
shimmen3141 <y.shin3141@gmail.com>
2025-03-16 16:36:33 +0900

    remove:不要なファイルの削除 #127

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,3 @@
-export { default as itemToTeaspoonGramsMap } from "./itemToTeaspoonGramsMap";
 export { default as radicalToKanjiMap } from "./radicalToKanjiMap";
-export { default as teaspoonGramsList } from "./teaspoonGramsList";
 export { default as seasoningList } from "./seasoningList";
 
