e0ce787522c572bf56bcf7d3e229636865b58765
shimmen3141 <y.shin3141@gmail.com>
2025-03-17 17:25:54 +0900

    update:ToggleSwitchArea内の処理をSETTINGS_CONFIGを用いるように修正 #131

--- src/features/Setting/components/ToggleSwitchArea.js
+++ src/features/Setting/components/ToggleSwitchArea.js
@@ -1,17 +1,23 @@
 import LabeledToggleSwitch from "./LabeledToggleSwitch";
+import { SETTINGS_CONFIG } from "../constants";
 import "./ToggleSwitchArea.scss";
 
 const ToggleSwitchArea = () => {
   console.log("ToggleSwitchArea");
+
+  const settings = Object.values(SETTINGS_CONFIG);
 
   return (
     <div className="toggleSwitchArea">
       <div className="triangleBorder"></div>
       <div className="triangle"></div>
       <div className="labeledToggleSwitches">
-        <LabeledToggleSwitch settingId="removeSymbols" />
-        <LabeledToggleSwitch settingId="spoonToGram" />
-        <LabeledToggleSwitch settingId="classifyItems" />
+        {settings.map((setting) => (
+          <LabeledToggleSwitch
+            key={setting.settingId}
+            settingId={setting.settingId}
+          />
+        ))}
       </div>
     </div>
   );
