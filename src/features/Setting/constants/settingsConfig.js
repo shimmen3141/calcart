d94dd2a1003a7a156f45a75186f36dbee63f1409
shimmen3141 <y.shin3141@gmail.com>
2025-03-17 16:52:08 +0900

    update:SETTINGS_CONFIGを修正 #131

--- src/features/Setting/constants/settingsConfig.js
+++ src/features/Setting/constants/settingsConfig.js
@@ -3,26 +3,20 @@ import { MODAL_CONFIG } from "modules";
 export const SETTINGS_CONFIG = {
   removeSymbols: {
     settingId: "removeSymbols",
-    label: "記号の消去",
+    labelText: "記号を消去",
     defaultToggle: true,
     relatedModalId: MODAL_CONFIG.removeSymbols.modalId,
   },
   spoonToGram: {
     settingId: "spoonToGram",
-    label: "大さじ・小さじ→グラムの変換",
+    labelText: "大さじ・小さじをグラムの変換",
     defaultToggle: true,
     relatedModalId: MODAL_CONFIG.spoonToGram.modalId,
   },
   classifyItems: {
     settingId: "classifyItems",
-    label: "リストの分割",
+    labelText: "リストを分割",
     defaultToggle: true,
     relatedModalId: MODAL_CONFIG.classifyItems.modalId,
-  },
-  default: {
-    settingId: "default",
-    label: "エラー",
-    defaultToggle: false,
-    relatedModalId: MODAL_CONFIG.default.modalId,
   },
 };
