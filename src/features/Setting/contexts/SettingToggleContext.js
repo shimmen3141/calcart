2d3aafbae42155fb41a666cb31aa43fbac84c2f1
shimmen3141 <y.shin3141@gmail.com>
2025-03-17 17:09:34 +0900

    update:settingTogglesの初期状態をSETTINGS_CONFIGから取得 #131

--- src/features/Setting/contexts/SettingToggleContext.js
+++ src/features/Setting/contexts/SettingToggleContext.js
@@ -1,15 +1,22 @@
 import React, { createContext, useContext, useState } from "react";
+import { SETTINGS_CONFIG } from "../constants";
 
 // Context の作成
 const SettingToggleContext = createContext();
 
+// SETTINGS_CONFIG をもとに初期状態を生成する
+const getInitialSettingToggles = () => {
+  return Object.values(SETTINGS_CONFIG).reduce((acc, setting) => {
+    acc[setting.settingId] = setting.defaultToggle;
+    return acc;
+  }, {});
+};
+
 // Context プロバイダの作成
 export const SettingToggleProvider = ({ children }) => {
-  const [settingToggles, setSettingToggles] = useState({
-    removeSymbols: true,
-    spoonToGram: true,
-    classifyItems: true,
-  });
+  const [settingToggles, setSettingToggles] = useState(
+    getInitialSettingToggles()
+  );
 
   const handleToggleChange = (id) => {
     setSettingToggles((prevStates) => ({
